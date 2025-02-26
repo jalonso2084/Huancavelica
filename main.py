d35c5c2e329016699a8291b87bb0f9349ccc1816
jalonso0780 <j.orgealonso24l@gmail.com>
2025-02-26 15:02:42 -0800

    Fix: Use Render's  environment variable

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/", include_in_schema=False)
-@app.head("/")
-async def read_root():
+@app.get("/")
+async def root():
     return {"message": "FastAPI is running!"}
 
-
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT not set
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
