8a948192291061d135595463d5a6b8d1f5a69dc1
jalonso0780 <j.orgealonso24l@gmail.com>
2025-02-26 18:58:18 -0800

    Fix: Removed incorrect 'methods' argument from @app.get()

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/", response_model=dict, methods=["GET", "HEAD"])
+@app.get("/", response_model=dict)
+@app.head("/", response_model=dict)
 async def read_root():
     return {"message": "FastAPI is running!"}
-
-
