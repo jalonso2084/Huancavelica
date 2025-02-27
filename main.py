de0fbd010e6b071268b5552c21355889e73b5d20
jalonso0780 <j.orgealonso24l@gmail.com>
2025-02-26 18:44:47 -0800

    Updated to use Render's default port

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class RootResponse(BaseModel):
-    message: str
-
-@app.get("/", response_model=RootResponse)
+@app.get("/")
 def read_root():
     return {"message": "FastAPI is running!"}
